7c95b99615d7f1284baa07e0db37545014e91e9d
Nic Roland <nicroland9@gmail.com>
2018-01-27 15:10:26 +0000

    Renamed some of the functions/fields

--- main.go
+++ main.go
@@ -11,19 +11,19 @@ import (
 // -----
 
 type Beacon struct {
-	UUID      string `json:"uuid"`
-	MAC       string `json:"mac"`
-	Name      string `json:"name"`
-	StatusURL string `json:"status-url,omitempty"`
+	UUID       string `json:"uuid"`
+	MAC        string `json:"mac"`
+	Name       string `json:"name"`
+	SessionURL string `json:"session-url,omitempty"`
 }
 
 func (b *Beacon) Register() {
-	uri := fmt.Sprintf("/status/%s", b.UUID)
-	status, err := url.Parse(uri)
+	uri := fmt.Sprintf("/session/%s", b.UUID)
+	session, err := url.Parse(uri)
 	if err != nil {
-		b.StatusURL = ""
+		b.SessionURL = ""
 	} else {
-		b.StatusURL = status.Path
+		b.SessionURL = session.Path
 	}
 }
 
@@ -41,19 +41,19 @@ func Homepage(w http.ResponseWriter, r *http.Request) {
 
 // -----
 
-type RegisterRequest struct {
+type FindRequest struct {
 	Beacon Beacon `json:"beacon"`
 }
 
-type RegisterResponse struct {
+type FindResponse struct {
 	Error   bool   `json:"error"`
 	Message string `json:"message"`
 	Beacon  Beacon `json:"beacon"`
 }
 
-func RegisterBeacon(w http.ResponseWriter, r *http.Request) {
+func FindBeacon(w http.ResponseWriter, r *http.Request) {
 	// TODO: Ensure this endpoint only accepts POSTs
-	var req RegisterRequest
+	var req FindRequest
 
 	// Check request for body
 	if r.Body == nil {
@@ -77,7 +77,7 @@ func RegisterBeacon(w http.ResponseWriter, r *http.Request) {
 	log.Println(req)
 
 	// Return a response
-	resp := RegisterResponse{Error: false, Message: "Beacon registered!", Beacon: req.Beacon}
+	resp := FindResponse{Error: false, Message: "Session created!", Beacon: req.Beacon}
 	json.NewEncoder(w).Encode(resp)
 }
 
@@ -85,6 +85,6 @@ func RegisterBeacon(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", Homepage)
-	http.HandleFunc("/register/", RegisterBeacon)
+	http.HandleFunc("/find/", FindBeacon)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
